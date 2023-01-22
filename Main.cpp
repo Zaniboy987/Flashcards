e06c8b8ab6f714f239d6b23ca91fd779022880a5
Zaniboy987 <nazarazan13@gmail.com>
2023-01-21 22:37:34 -0500

    added initial stage of user_input

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 using namespace std;
 
+// 
+class user_input {
+    public:
+        string output = "This program works!";
+    void print_out(){
+        cout << output << endl;
+    }
+};
 
 
 int main(int argc, char *argv[]) {
